e297624fae1ca2ea984b261f25c354ced45024d9
Islam Jahedul <jahedul.islam@welldev.io>
2024-05-23 16:43:03 +0600

    initial commit

---
+++ src/main/java/com/jahedul/busybox_backend/BusyboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jahedul.busybox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusyboxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jahedul/busybox_backend/BusyboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jahedul.busybox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusyboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
