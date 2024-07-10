d692195c3da63634e8da6bd7fb8c22f0b6752312
jahedul <jahedulislam6060@gmail.com>
2024-07-10 22:52:14 +0600

    change url

--- src/main/java/com/jahedul/busybox_backend/constants/Url.java
+++ src/main/java/com/jahedul/busybox_backend/constants/Url.java
@@ -1,7 +1,7 @@
 package com.jahedul.busybox_backend.constants;
 
 public class Url {
-  public static final String ROOT = "/busybox";
+  public static final String ROOT = "/busyBox";
   public static final String API_V1 = ROOT + "/api/v1";
   public static final String PING = "/ping";
 }
